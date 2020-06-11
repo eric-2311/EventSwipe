0fd54880b0066585baa8d575c9b9a95ce9617c9f
elee <ericlee.2311@gmail.com>
2020-06-11 10:58:11 -0400

    styled ticket modal and added github linkedin links

--- frontend/event_swipe.jsx
+++ frontend/event_swipe.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab)
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
